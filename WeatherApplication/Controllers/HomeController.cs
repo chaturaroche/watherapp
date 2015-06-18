469297ece9631c7110f8c974d721dc04d5afc97c
chaturaroche <chaturaroche@gmail.com>
2015-06-18 07:49:33 +0530

    master
    asas

---
+++ WeatherApplication/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeatherApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[Authorize]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "UK daily forecast and weekly forecast";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "UK weather Forecast Application Description";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Chatura Roche";
+
+            return View();
+        }
+    }
+}
